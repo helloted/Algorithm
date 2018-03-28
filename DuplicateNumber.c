0728dd30716a9a2853136f83aaf9c502f782254c
iMac <helloted@qq.com>
2018-03-28 17:49:03 +0800

    update

--- DuplicateNumber.c
+++ DuplicateNumber.c
@@ -33,6 +33,35 @@ int method_1(int data[],int length){
 }
 
 
+void quick(int data[],int l, int r){
+    int i = l;
+    int j = r;
+    int temp = data[i]; //l位置也就是i位置挖空,数值作为分化的标记。i位置现在为空
+    while(i< j){
+        while(i<j && data[j] > temp){
+            j--;
+        }
+        if (i<j)
+        {
+            data[i] = data[j]; //将j位置小于temp的值，填入上一个挖空的i位置，j位置现在为空。
+            i++;
+        }
+
+        while(i<j && data[i] < temp){
+            i ++;
+        }
+        if (i<j)
+        {
+            data[j] = data[i];
+            j--;
+        }
+    }
+    data[i] = temp;
+    quick(data,l,i-1);
+    quick(data,i+1,r);
+}
+
+
 
 /* 
 	快排之后，值与位置不对应的就是重复的
@@ -50,12 +79,15 @@ int method_2(int data[],int length){
 }
 
 
+/* 
+	快排的中心思想，是将数组切割为两个，左边小于，右边大于，然后再分别递归
+*/
 void quick_sort(int s[],int l,int r)
 {
     if(l < r)
     {
         int i=l,j=r,x=s[l];
-        while(i<j)
+        while(i<j) //从i,j两边向中间靠拢，将小于的值放到左边，大于的值放到右边
         {
             while(i<j && s[j]>=x)//从右到左找到第一个小于x的数  
                 j--;
@@ -65,11 +97,10 @@ void quick_sort(int s[],int l,int r)
             while(i<j && s[i]<=x)//从左往右找到第一个大于x的数  
                 i++;
             if(i<j)
-                s[j--]=s[i]; 
-            
+                s[j--]=s[i];      
         } 
-        s[i]=x;//i = j的时候，将x填入中间位置  
-        quick_sort(s,l,i-1);//递归调用 
+        s[i]=x;	//i = j的时候，将x填入中间位置  
+        quick_sort(s,l,i-1);	//递归调用 
         quick_sort(s,i+1,r);        
     }
 }
